777f1a0ad3fe276841b00b98b512f6f794d95ffb
Trever <tkjones123456@gmail.com>
2021-10-13 12:56:38 -0400

    added font awesome Icons added profile dropdown if user is logged in added post detail view added ability to edit and delete posts add authentication for edit and delete pages, only correct user can access

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
 ]

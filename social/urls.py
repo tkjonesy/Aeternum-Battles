a2a48d57e4260918f945e74c39f4fb4f85dd7fa2
Trever <tkjones123456@gmail.com>
2021-10-17 03:02:42 -0400

    Added Profile and Profile Edit Added database to commit

--- social/urls.py
+++ social/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, ProfileView, ProfileEditView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
 ]
--- social/views.py
+++ social/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
-from .models import Post
+from .models import Post, UserProfile
 from .forms import PostForm
 from django.views.generic.edit import UpdateView, DeleteView
 
@@ -69,4 +69,32 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+
+class ProfileView(View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        user = profile.user
+        posts = Post.objects.filter(author=user).order_by('-created_on')
+
+        context = {
+            'user': user,
+            'profile': profile,
+            'posts': posts,
+        }
+        return render(request, 'social/profile.html', context)
+
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = UserProfile
+    fields = ['name', 'bio', 'picture']
+    template_name = 'social/profile_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('profile', kwargs={'pk': pk})
+
+    def test_func(self):
+        profile = self.get_object()
+        return self.request.user == profile.user
 # Create your views here.

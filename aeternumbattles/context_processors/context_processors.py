c66ab7ea6a9828971760133063fee6be9a61f150
Andrew <marottaandrew1@gmail.com>
2021-10-30 16:14:00 -0400

    made notification bell display properly

--- aeternumbattles/context_processors/context_processors.py
+++ aeternumbattles/context_processors/context_processors.py
@@ -1,20 +1,24 @@
 from social.models import FriendRequest, UserProfile
+from django.contrib.auth.models import User
+
 
 
 def context_processor(request):
     if hasattr(request, 'user'):
         if hasattr(request.user, 'profile'):
             profile = request.user.profile
+            print(request.user.profile)
 
             friend_requests = FriendRequest.objects.filter(receiver=profile).order_by('-timestamp')
 
             has_friend_requests = True if friend_requests else False
+            print(has_friend_requests)
 
             data = {
-                'friend_requests': has_friend_requests,
+                'has_friend_requests': has_friend_requests,
             }
             return data
 
     return {
-                'friend_requests': False,
+                'has_friend_requests': False,
             }

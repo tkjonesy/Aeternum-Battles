e79b139c3c2bf9d15c709b21ac50d13137b7cbb3
tkjonesy <tkjones123456@gmail.com>
2021-11-19 00:17:15 -0500

    Reworked Notification System

--- social/admin.py
+++ social/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, UserProfile, FriendRequest
+from .models import Post, UserProfile, FriendRequest, Notifications
 
 admin.site.register(Post)
 admin.site.register(UserProfile)
 admin.site.register(FriendRequest)
+admin.site.register(Notifications)

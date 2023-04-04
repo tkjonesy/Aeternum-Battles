9d1d31f153bba1a7150aae655b21ce95f6c0b440
Trever Jones <73763478+tkjonesy@users.noreply.github.com>
2023-04-03 20:50:11 -0400

    Update settings.py

--- aeternumbattles/settings.py
+++ aeternumbattles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nbs_7%2zsu5yj34l)czmp%^6^c2*5%2!8kup+wv@l5$vfgh-yi'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
